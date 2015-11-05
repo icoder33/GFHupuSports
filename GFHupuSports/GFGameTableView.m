782994cf6e0906186ca13a83c7484fc7daa8b6e5
guangfu <guangfu.wang@huaban.com>
2015-11-06 00:17:06 +0800

    添加game界面

---
+++ GFHupuSports/GFGameTableView.m
@@ -0,0 +1,21 @@
+//
+//  GFGameTableView.m
+//  GFHupuSports
+//
+//  Created by wanggf on 15/11/6.
+//  Copyright © 2015年 wanggf. All rights reserved.
+//
+
+#import "GFGameTableView.h"
+
+@implementation GFGameTableView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
